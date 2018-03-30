7eef6408d5427781c11b15392a7d815fcac4919e
yanghaiwei <3460399846@qq.com>
2018-03-30 09:46:59 +0800

    zxk control.sql

--- config/control.sql
+++ config/control.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50535
 File Encoding         : 65001
 
-Date: 2018-01-26 16:44:03
+Date: 2018-03-30 09:46:28
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,13 +21,18 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `application`;
 CREATE TABLE `application` (
   `id` int(11) NOT NULL,
-  `computerId` int(11) NOT NULL,
+  `computer_id` int(11) NOT NULL,
   `application_name` varchar(50) NOT NULL,
   `application_url` varchar(50) NOT NULL,
-  `createTime` datetime NOT NULL,
-  `isClose` int(11) NOT NULL,
+  `create_time` datetime NOT NULL,
+  `isclose` int(11) NOT NULL,
+  `type` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of application
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for computer
@@ -47,6 +52,11 @@ CREATE TABLE `computer` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Records of computer
+-- ----------------------------
+INSERT INTO `computer` VALUES ('1', '11', '192.168.254.47', '2018-01-25 15:41:36', 'MTIzNDU2', '2018-01-25 15:41:36', 'MTExNTE2ODY2MDk2Njc3', '18888', '192.168.254.47');
+
+-- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
@@ -58,3 +68,16 @@ CREATE TABLE `user` (
   `epassword` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'zxk', '15603998400', '123456', '123456');
+INSERT INTO `user` VALUES ('2', 'str', '15603998400', '123456', '123456');
+INSERT INTO `user` VALUES ('3', 'qwe', '15603998400', '132456', '132456');
+INSERT INTO `user` VALUES ('4', 'werew', '15603998400', '132456', '132456');
+INSERT INTO `user` VALUES ('5', 'ssdfs', '15603998400', '132456', '132456');
+INSERT INTO `user` VALUES ('6', 'sdfdsfsd', '15603998400', '1234526', '123456');
+INSERT INTO `user` VALUES ('7', 'poi', '15603998400', '132456', '123456');
+INSERT INTO `user` VALUES ('8', 'sdlfjs', '15603998400', '123456', '132456');
+INSERT INTO `user` VALUES ('9', 'mysql', '46547', '123456', '13214564');
